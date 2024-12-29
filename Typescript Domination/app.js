acbceaca5f17c5df24b33d00d4f67ed06e2fd9f6
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-29 09:11:44 +0530

    code

--- Typescript Domination/app.js
+++ Typescript Domination/app.js
@@ -10,19 +10,55 @@
 // 1. Primitives -- Simple Types -- Special Types 
 // 2. Non Primitives - Reference 
 // Primitives Data Types 
-var boolean = true;
-var number = 13;
-var string = "sethiji";
-// bigInt, Symbol, Any, Unknown, Never, Undefined, Null, Void
+// Any, Number, Null, Never, Boolean, BigInt, String, Symbol, Unknown, Undefined, Void
+var type1; // Any
+var type2 = 13; // Number
+var type3 = null; // Null   
+var type5 = true; // Boolean        
+var type6; // BigInt            
+var type7 = "raghav"; // String            
+var type8; // Symbol
+var type9; // Unknown
+var type10 = undefined; // Undefined    
+// Void --> never return value this code
+function type11() {
+    console.log("Hey");
+}
+// Never --> never run this code 
+function type4() {
+    while (true) { }
+}
 // Array 
 var arr1 = [1, 2, 3];
 var arr2 = [1, 2, 3, true, "raghav"];
 var arr3 = [1, 2, 3, true, "raghav", { fullname: "raghavsethi" }];
-
-// enum - enumerations
+var arr4 = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+// Tuples 
+var arr5 = ["raghav", 18];
+var arr6 = ["raghav", 18, true];
+// Objects
+var obj1 = { type: "Toyota", model: "Fortuner", year: 2025 };
+// Enum - Enumerations
 var StatusCode;
 (function (StatusCode) {
     StatusCode["NOTFOUND"] = "not found status code 404";
     StatusCode["SUCCESS"] = "succes status code 200";
 })(StatusCode || (StatusCode = {}));
 console.log(StatusCode.SUCCESS);
+// Interfaces
+var inter1 = 12; //this is a number --> its upon on value of data
+var inter2 = "13"; //this is a string --> its upon on value of data
+var inter3 = true; //this is a boolean --> its upon on value of data
+function getDataOfUser(obj) {
+    obj.name;
+    obj.email;
+    obj.password;
+    obj.gender;
+}
+getDataOfUser({ name: 'raghav', email: 'raghavsethi@gmail.com', password: 'raghav123' });
+// Annotations
+var anno1 = 13; // defined the type of data 
+var anno2 = "raghav"; // defined the type of data 
+var anno3 = true; // defined the type of data 
+// Union
+var union = "raghavsethiji";
--- Typescript Domination/app.ts
+++ Typescript Domination/app.ts
@@ -14,25 +14,77 @@
 // 2. Non Primitives - Reference 
 
 // Primitives Data Types 
-let boolean = true
-let number = 13
-let string = "sethiji"
-// bigInt, Symbol, Any, Unknown, Never, Undefined, Null, Void
+// Any, Number, Null, Never, Boolean, BigInt, String, Symbol, Unknown, Undefined, Void
+
+let type1;                      // Any
+let type2: number = 13;          // Number
+let type3: null = null;         // Null   
+let type5: boolean = true;        // Boolean        
+let type6: bigint;  // BigInt            
+let type7: string  = "raghav";    // String            
+let type8: symbol;              // Symbol
+let type9:unknown;            // Unknown
+let type10: undefined = undefined; // Undefined    
+  
+// Void --> never return value this code
+function type11(): void{
+    console.log("Hey")
+}
+
+// Never --> never run this code 
+function type4(): never{
+    while(true){}
+}
 
 // Array 
 let arr1 = [1, 2, 3]
 let arr2 = [1, 2, 3, true, "raghav"]
-let arr3 = [1, 2, 3, true, "raghav", {fullname: "raghavsethi"}]
-// Typescript Array 
+let arr3 = [1, 2, 3, true, "raghav", {fullname: "raghavsethi"}] 
 let arr4: number[] = [1, 2, 4, 5, 6, 7, 8, 9, 10]
 
-// Typescript Tuples 
+// Tuples 
 let arr5:[string, number] = ["raghav", 18]
 let arr6:[string, number, boolean] = ["raghav", 18, true]
 
-// enum - enumerations
+// Objects
+const obj1: { type: string, model: string, year: number } = {type: "Toyota", model: "Fortuner", year: 2025};
+
+// Enum - Enumerations
 enum StatusCode {
     NOTFOUND = "not found status code 404",
     SUCCESS = "succes status code 200"
 }
 console.log(StatusCode.SUCCESS)
+
+// Interfaces
+
+let inter1 =  12   //this is a number --> its upon on value of data
+let inter2 = "13"  //this is a string --> its upon on value of data
+let inter3 = true  //this is a boolean --> its upon on value of data
+
+interface User{
+    name: string,       // bina diya code run nahi hoga 
+    email: string,      // bina diya code run nahi hoga 
+    password: string,   // bina diya code run nahi hoga    
+    gender?: string     // Optional hai do ya, mat do koi farak nahi padta 
+}
+
+function getDataOfUser(obj: User){
+    obj.name
+    obj.email
+    obj.password
+    obj.gender
+}
+
+getDataOfUser({name: 'raghav', email: 'raghavsethi@gmail.com', password: 'raghav123'})
+
+
+
+// Annotations
+let anno1: number = 13         // defined the type of data 
+let anno2: string = "raghav"   // defined the type of data 
+let anno3: boolean = true      // defined the type of data 
+
+// Union
+let union: number | string | boolean = "raghavsethiji"
+
